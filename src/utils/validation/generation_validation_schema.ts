f10ee66674b1d7cd639f82d55936cf71a49ce912
jthornaday <jakehornaday@gmail.com>
2026-08-28 14:18:52 -0700

    Allow five cuteness levels

--- src/utils/validation/generation_validation_schema.ts
+++ src/utils/validation/generation_validation_schema.ts
@@ -8,7 +8,7 @@ const generateImageSchema = Zod.object({
   })
     .min(1)
     .max(4),
-  cutenessLevel: Zod.number().int().min(1).max(3).default(2),
+  cutenessLevel: Zod.number().int().min(1).max(5).default(2),
 });
 
 export { generateImageSchema };
