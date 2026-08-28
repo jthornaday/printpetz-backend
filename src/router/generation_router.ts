084e5e0908486b502209f7987ea424cff0a9404b
jthornaday <jakehornaday@gmail.com>
2026-08-28 15:52:11 -0700

    Add background removal route

--- src/router/generation_router.ts
+++ src/router/generation_router.ts
@@ -5,5 +5,6 @@ import * as generationController from "@/controllers/generation_controller";
 const router: Router = Router();
 
 router.post("/create", generationController.createImage);
+router.post("/remove-background", generationController.removeBackground);
 
 export default router;
