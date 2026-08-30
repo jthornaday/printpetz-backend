db72bb7beab7edf5d3cea4fc2e0a2dd3f38b02a1
jthornaday <jakehornaday@gmail.com>
2026-08-30 09:24:05 -0700

    Expose generation file download endpoint

--- src/router/generation_router.ts
+++ src/router/generation_router.ts
@@ -5,6 +5,7 @@ import * as generationController from "@/controllers/generation_controller";
 const router: Router = Router();
 
 router.post("/create", generationController.createImage);
+router.get("/:id/download", generationController.downloadImage);
 router.post("/edit-look", generationController.editLook);
 router.post("/remove-background", generationController.removeBackground);
 
